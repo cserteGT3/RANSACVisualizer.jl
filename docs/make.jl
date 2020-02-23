bfa8bc7427cc5c4655e6a3439cead6d5c2a71ee7
Tamás Cserteg <csertegt@gmail.com>
2020-02-23 20:29:21 +0100

    buid docs

---
+++ docs/make.jl
@@ -0,0 +1,7 @@
+using Documenter
+
+makedocs(sitename="RANSACVisualizer.jl",
+        pages=["Home" => "index.md"])
+
+
+deploydocs(repo="github.com/cserteGT3/RANSACVisualizer.jl.git")
---
+++ docs/src/index.md
@@ -0,0 +1,4 @@
+# RANSACVisualizer.jl
+
+More to come...
+Until then, check out the docs of [RANSAC.jl](https://github.com/cserteGT3/RANSAC.jl)
