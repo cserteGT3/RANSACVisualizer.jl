f1206830e01e044048f1e50368aa10b859fe989f
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-03-01 12:59:05 +0000

    build based on eb21527

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#RANSACVisualizer.jl-1","page":"Home","title":"RANSACVisualizer.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"More to come... Until then, check out the docs of RANSAC.jl","category":"page"}]
+}
